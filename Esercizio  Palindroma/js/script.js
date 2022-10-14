06bfc4c8bc4b50babe1f544f6e2d8505ffb94e2f
GiuseppeLanucara <giuseppelanucara96@gmail.com>
2022-10-14 16:48:20 +0200

    provando a interpretare funzione

---
+++ Esercizio  Palindroma/js/script.js
@@ -0,0 +1,18 @@
+/* **Palindroma**
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma. */
+
+const parolaUtente = prompt("Ciao dimmi una parola a caso.")
+console.log(parolaUtente);
+
+/**
+ * Description Questa funzione controlla se la parola inserita è palindroma
+ * @param {string} lettereParola parola da controllare
+ * @returns {string} ritorna la scritta palindroma o non palindroma
+ */
+ function parolaPalindroma (lettereParola) {
+    let i = 0;
+    let f = parolaUtente.length - 1;
+
+    while (i= parolaUtente.length) {
+        i++;
