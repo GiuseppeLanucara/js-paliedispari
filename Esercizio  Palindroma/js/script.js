28a84ec6e7d36e582e08a0875c6a9b406b198b8a
GiuseppeLanucara <giuseppelanucara96@gmail.com>
2022-10-14 19:20:08 +0200

    provato a fare in maniera differente ma senza riuscirci

--- Esercizio  Palindroma/js/script.js
+++ Esercizio  Palindroma/js/script.js
@@ -2,38 +2,57 @@
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma. */
 
-let parolaUtente = [];
-parolaUtente = prompt ("Scrivi una parola qualsiasi");
-let parolaNormale = [];
-let parolaContrario = [];
+ let parolaUtente = [];
+ parolaUtente = prompt ("Scrivi una parola qualsiasi");
+ let parolaNormale = [];
+ let parolaContrario = [];
 
-if (parolaPalindroma(parolaUtente)) {
-    console.log("La parola è palindroma");
-    alert("La parola è palindroma")
-} else {
-    console.log("La parola non è palindroma ");
-    alert("La parola non è palindroma")
-}
+ if (parolaPalindroma(parolaUtente)) {
+     console.log("La parola è palindroma");
+     alert("La parola è palindroma")
+ } else {
+     console.log("La parola non è palindroma ");
+     alert("La parola non è palindroma")
+ }
 
 
-/**
- * Description: La funzione controlla se la parola è palindroma 
- * @param {string} parolaDaControllare parola da controllare 
- * @returns {boolean} è palindroma
- */
-//                          8
-function parolaPalindroma(parolaDaControllare) {
-    let ePalindroma = false;
-    parolaNormale = parolaDaControllare;
+ /**
+  * Description: La funzione controlla se la parola è palindroma 
+  * @param {string} parolaDaControllare parola da controllare 
+  * @returns {boolean} è palindroma*/
 
-    for (let j = parolaDaControllare.length - 1; j >= 0; j--) {
-        parolaContrario += parolaDaControllare[j];
-    }
+ 
+ function parolaPalindroma(parolaDaControllare) {
+     let ePalindroma = false;
+     parolaNormale = parolaDaControllare;
 
-    if (parolaNormale === parolaContrario) {
-        ePalindroma = true;
-    } else {
-        ePalindroma = false;
-    }
-    return ePalindroma
-}
+     for (let j = parolaDaControllare.length - 1; j >= 0; j--) {
+         parolaContrario += parolaDaControllare[j];
+     }
+
+     if (parolaNormale === parolaContrario) {
+         ePalindroma = true;
+     } else {
+         ePalindroma = false;
+     }
+     return ePalindroma
+ }
+
+// const parolaUtente = prompt("Scrivi una parola")
+// console.log(controlloPalindroma(parolaUtente));
+
+// function controlloPalindroma(parolaDaControllare) {
+        
+//         let sinistra = 0;
+//         let destra = parolaDaControllare.lenght-1;
+
+//         while (sinistra<destra) {
+//             if(parolaDaControllare[sinistra] !== parolaDaControllare[destra]) {
+//                 return  false;
+//             }
+//             ++sinistra;
+//             --destra;
+//         }
+
+//         return true  ;
+// }
